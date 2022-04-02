b366115c1efd66767eed522008f27a3050927edb
ShobrinaFathoniah <shobrinaf@gmail.com>
2022-04-02 07:32:25 +0700

    feat: add colors

---
+++ src/utils/colors.js
@@ -0,0 +1,10 @@
+export const WHITE = '#000000';
+export const BLACK = '#FFFFFF';
+
+export const LIGHT_BLUE_100 = '#79f5ff';
+export const LIGHT_BLUE_300 = '#39c2dd';
+export const LIGTH_BLUE_500 = '#0091ab';
+
+export const DARK_PURPLE_100 = '#8180b7';
+export const DARK_PURPLE_300 = '#535487';
+export const DARK_PURPLE_500 = '#272b5a';
